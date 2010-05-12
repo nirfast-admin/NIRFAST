b89702c521b3469b89f7ee176c3c3b25a25286da
michael.jermyn@dartmouth.edu <michael.jermyn@dartmouth.edu@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2010-05-12 14:33:42 +0000

    further improvements to move_detector, in case the detector is inside the mesh

--- toolbox/move_detector.m
+++ toolbox/move_detector.m
@@ -33,39 +33,62 @@ elseif size(mesh.elements,2) == 3
     if mesh.dimension == 3
         faces = mesh.elements(sum(mesh.bndvtx(mesh.elements),2)==3,:);
     elseif mesh.dimension == 2
-        faces = mesh.elements(sum(mesh.bndvtx(mesh.elements),2)==2,:);
+        faces = [mesh.elements(:,[1,2]);
+                  mesh.elements(:,[1,3]);
+                  mesh.elements(:,[2,3])];
+        faces = sort(faces,2);
+        faces = unique(faces,'rows');
+        faces = faces(sum(mesh.bndvtx(faces),2)==2,:);
     end
 end
 
 %% loop through detectors
 for i=1:size(mesh.meas.coord,1)
     
-    % find closest boundary node
     if mesh.dimension == 2
+        
+        % find closest boundary node
         dist = distance(mesh.nodes,mesh.bndvtx,[mesh.meas.coord(i,:) 0]);
+        r0_ind = find(dist==min(dist));
+        r0_ind = r0_ind(1);
+        
+        % find faces including the closest boundary node
+        fi = faces(sum(faces==r0_ind,2)>0,:);
+
+        % find closest face
+        dist = zeros(size(fi,1),1);
+        point = zeros(size(fi,1),3);
+        for j=1:size(fi,1)
+            [dist(j),point(j,:)] = pointLineDistance(mesh.nodes(fi(j,1),:), ...
+                mesh.nodes(fi(j,2),:),mesh.meas.coord(i,:));
+        end
+        smallest = find(dist == min(dist));
+        
+        % move detector to the closest point on that face
+        mesh.meas.coord(i,1:2) = point(smallest(1),1:2);
+        
     elseif mesh.dimension == 3
+
+        % find closest boundary node
         dist = distance(mesh.nodes,mesh.bndvtx,mesh.meas.coord(i,:));
-    end
-    r0_ind = find(dist==min(dist));
-    r0_ind = r0_ind(1);
+        r0_ind = find(dist==min(dist));
+        r0_ind = r0_ind(1);
+
+        % find faces including the closest boundary node
+        fi = faces(sum(faces==r0_ind,2)>0,:);
+
+        % find closest face
+        dist = zeros(size(fi,1),1);
+        point = zeros(size(fi,1),3);
+        for j=1:size(fi,1)
+            [dist(j),point(j,:)] = pointTriangleDistance([mesh.nodes(fi(j,1),:);...
+                mesh.nodes(fi(j,2),:);mesh.nodes(fi(j,3),:)],mesh.meas.coord(i,:));
+        end
+        smallest = find(dist == min(dist));
+        
+        % move detector to the closest point on that face
+        mesh.meas.coord(i,1:3) = point(smallest(1),1:3);
     
-    % find faces including the closest boundary node
-    fi = faces(sum(faces==r0_ind,2)>0,:);
-
-    % find closest face
-    dist = zeros(size(fi,1),1);
-    point = zeros(size(fi,1),3);
-    for j=1:size(fi,1)
-        [dist(j),point(j,:)] = pointTriangleDistance([mesh.nodes(fi(j,1),:);...
-            mesh.nodes(fi(j,2),:);mesh.nodes(fi(j,3),:)],mesh.meas.coord(i,:));
-    end
-    smallest = find(dist == min(dist));
-
-    % move detector to the closest point on that face
-    if size(mesh.meas.coord,2) == 2
-        mesh.meas.coord(i,:) = point(smallest(1),1:2);
-    elseif size(mesh.meas.coord,2) == 3
-        mesh.meas.coord(i,:) = point(smallest(1),:);
     end
         
 end
