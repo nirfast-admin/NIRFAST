a8777df3dbffa3cb99e877ded2561dc8a5ceecf0
hamiid <hamiid@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2010-06-11 23:51:33 +0000

    separated the surface relation extraction from inp2nirfast and added to a standalone module.

--- toolbox/meshing/inp2nirfast_bem.m
+++ toolbox/meshing/inp2nirfast_bem.m
@@ -22,49 +22,9 @@ for i=1:no_regions
     fn = [fnprefix num2str(i) '.inp'];
     abaqus2nodele_surface(fn);
 end
-fprintf('\tSurface detection.\n\t\t');
-surface_relations_mex(fnprefix,no_regions);
-relations = textread('surface_relations.txt');
 
-region_id=zeros(no_regions,1);
-idcounter=1;
-for i=1:size(relations,1)
-    if i==1
-        fn = [fnprefix num2str(relations(i,1))];
-        [mesh.elements,mesh.nodes] = read_nod_elm(fn,1);
-        region1 = repmat(1,[size(mesh.elements,1) 1]);
-        region2 = repmat(0,[size(mesh.elements,1) 1]);
-        no_ext_nodes = size(mesh.nodes,1);
-        NoBdyNodes(i) = no_ext_nodes;
-        NoBdyElems(i) = size(mesh.elements,1);
-    end
-    if region_id(relations(i,1)) == 0
-        region_id(relations(i,1)) = idcounter;
-        idcounter = idcounter + 1;
-    end
-    momid = region_id(relations(i,1));
-    for j=2:size(relations,2)
-        if relations(i,j)==0, continue; end
-            fn = [fnprefix num2str(relations(i,j))];
-            kidid = idcounter;
-            region_id(relations(i,j)) = idcounter;
-            
-            [fooele,foonode] = read_nod_elm(fn,1);
-            mesh.elements=[mesh.elements;fooele+size(mesh.nodes,1)]; %#ok<AGROW>
-            mesh.nodes=[mesh.nodes; foonode]; %#ok<AGROW>
-            region1 = cat(1,region1,repmat(momid,[size(fooele,1) 1]));
-            region2 = cat(1,region2,repmat(kidid,[size(fooele,1) 1]));
-            
-            NoBdyNodes(idcounter) = size(foonode,1);
-            NoBdyElems(idcounter) = size(fooele,1);
-            idcounter = idcounter + 1;
-    end
-end
-foo=zeros(size(mesh.nodes,1),1);
-foo(1:no_ext_nodes,:) = 1;
-
-mesh.bndvtx = foo;
-mesh.region = [region1 region2];
+%% Compute surface relations
+mesh = ExtractSurfaceRelations(fnprefix, no_regions);
 
 %% write nirfast mesh
 fprintf('\tWriting nirfast mesh files\n');
