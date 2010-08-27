4e56359ab456a6c7e140b72eec8aeb914f39a3d4
venkatu <venkatu@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2010-08-27 15:13:03 +0000

    added explicit iparm() definition to workaround situations where OMP_NUM_THREADS is not honored

--- toolbox/solve_pardiso.m
+++ toolbox/solve_pardiso.m
@@ -1,22 +1,27 @@
 function phi = solve_pardiso(MASS,qvec,solvertype)
 % phi = solve_pardiso(MASS,qvec,solvertype) solves a complex, non-symmetric
-% system of linear equations with multiple RHS terms using a mex'd version
-% of UBasel's PARDISO solver library. MASS is the sparse
-% system matrix, qvec is the sparse RHS matrix and phi is a sparse
-% solution. solvertype should be 0 for direct solve and 1 for iterative
-% solve. Visit www.pardiso-project.org for more details on the solver.
-% nirfast_pardiso_readme.txt has details on how to setup the linux
-% environment to run the solver through matlab.
+% system of linear equations with multiple RHS terms using the matlab interface
+% of UBasel's PARDISO solver library. MASS is the sparse system matrix, qvec 
+% is the sparse RHS matrix and phi is a sparse solution. solvertype should 
+% be 0 for direct solve and 1 for iterative solve. 
 %
-% venkat krishnaswamy, 08/25/2010
+% Visit www.pardiso-project.org for more details on the solver.
+%
+% venkat krishnaswamy, 08/27/2010
 
 %nc=System.Environment.ProcessorCount;
 %system(['export OMP_NUM_THREADS =' num2str(nc)]);
 
 fullqvec = full(qvec); %NIRFAST stores the RHS matrix as sparse but PARDISO wants it as full
 info = pardisoinit(13,solvertype);
+
+% set num of comp threads explicitly to workaround some situations where
+% OMP_NUM_THREADS is not honored
+info.iparm(3) = 8;
+ 
 info = pardisoreorder(MASS,info,false);
 [fullphi, info] = pardisosolve(MASS,fullqvec,info,false);
 phi = sparse(fullphi); %PARDISO will return a full solution matrix but NIRFAST wants it sparse
+
 pardisofree(info);
 clear info;
