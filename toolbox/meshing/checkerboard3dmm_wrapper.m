38b64b6a7b04b7406fbef3f0670b62a5f6998b6e
hamiid <hamiid@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2010-09-30 07:19:05 +0000

    the wrapper for checkerboard3d now accepts edgesize as its 4th argument.

--- toolbox/meshing/checkerboard3dmm_wrapper.m
+++ toolbox/meshing/checkerboard3dmm_wrapper.m
@@ -1,16 +1,17 @@
-function checkerboard3dmm_wrapper(fnprefix,saveloc,type)
+function checkerboard3dmm_wrapper(fnprefix,saveloc,type,edgesize)
 
-% checkerboard3dmm_wrapper(fnprefix,saveloc,type)
+% checkerboard3dmm_wrapper(fnprefix,saveloc,type,edgesize)
 %
 % Converts inp files (surface mesh) to a fem nirfast mesh
 %
 % fnprefix is the location of one of the inp files, or the ele file location
 % saveloc is the location to save the mesh to
 % type is the mesh type ('stnd', 'fluor', etc)
+% edgesize is the average size of tetrahedra edges
 
 
 %% elements, nodes, region, bndvtx, dimension, type, name
-mesh = checkerboard3d_mm(fnprefix,type);
+mesh = checkerboard3d_mm(fnprefix,type,edgesize);
 
 %% optical properties
 mesh = set_mesh_type(mesh,type);
