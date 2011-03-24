01a000a2cd14732895256499e6bb9033b4c1f9b9
hamiid <hamiid@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2011-03-24 21:31:05 +0000

    Fixed a bug in which nirfast could not detect extra information if the .node/.ele file had region properties already in it.

--- toolbox/meshing/nodelm2nirfast.m
+++ toolbox/meshing/nodelm2nirfast.m
@@ -10,17 +10,39 @@ function nodelm2nirfast(fn,saveloc,type)
 
 
 %% read .node/.ele files
-[mesh.elements,mesh.nodes] = read_nod_elm(fn(1:end-4),1);
+[mesh.elements,mesh.nodes,nodemap,elemap,dim,nnpe] = read_nod_elm(fn(1:end-4),1);
+
 
 %% set mesh properties
 mesh = set_mesh_type(mesh,type);
 if strcmp(type,'stnd_bem') || strcmp(type,'fluor_bem') || strcmp(type,'spec_bem')
-    mesh.region = [ones(size(mesh.elements,1),1) zeros(size(mesh.elements,1),1)];
+    if size(mesh.elements,2)>=nnpe+2
+        mesh.region = mesh.elements(:,nnpe+1:nnpe+2);
+        mesh.elements = mesh.elements(:,1:nnpe);
+    else
+        mesh.region = [ones(size(mesh.elements,1),1) zeros(size(mesh.elements,1),1)];
+    end
     mesh.dimension = 3;
     mesh.bndvtx = ones(size(mesh.nodes,1),1);
 else
-    mesh.region = zeros(size(mesh.nodes,1),1);
-    mesh.dimension = size(mesh.elements,2)-1;
+    if size(mesh.elements,2)>nnpe
+        % Get region info
+        % Since NIRFAST is a node based FEM package we need to assign
+        % material/region IDs to nodes rather than elements!
+        region_ids = unique(mesh.elements(:,nnpe+1));
+        if min(region_ids)==0, region_ids=region_ids+1; end
+        mesh.region = zeros(size(mesh.nodes,1),1);
+        for i=1:length(region_ids)
+            relem = mesh.elements(mesh.elements(:,nnpe+1)==region_ids(i),:);
+            foo = relem(:,1:nnpe);
+            rnodes = unique(foo(:));
+            mesh.region(rnodes) = region_ids(i);
+        end
+        mesh.elements=mesh.elements(:,1:4);
+    else
+        mesh.region = zeros(size(mesh.nodes,1),1);
+    end
+    mesh.dimension = dim;
 end
 
 faces=[mesh.elements(:,[1,2,3]);
