b33330350d315522d5d848d48c377a62c2717862
jermynmichael@gmail.com <jermynmichael@gmail.com@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2012-04-13 17:01:36 +0000

    fix for the legend when plotting a mesh with few sources

--- toolbox/plotmesh.m
+++ toolbox/plotmesh.m
@@ -29,13 +29,13 @@ if plotflag == 1
             if length(tmp)>1
                 s1 = mesh.source.coord(mesh.source.num == tmp(1),:);
                 s2 = mesh.source.coord(mesh.source.num == tmp(2),:);
+                plot(s1(:,1),s1(:,2),'go',s2(:,1),s2(:,2),'yo',...
+                    mesh.source.coord(3:end,1),...
+                    mesh.source.coord(3:end,2),'ro','LineWidth',2,'MarkerSize',8);
             else
-                s1 = mesh.source.coord; s2 = s1;
-            end
-            plot(s1(:,1),s1(:,2),'go',s2(:,1),s2(:,2),'yo',...
-                mesh.source.coord(3:end,1),...
-                mesh.source.coord(3:end,2),'ro','LineWidth',2,'MarkerSize',8);
-            
+                s1 = mesh.source.coord;
+                plot(s1(:,1),s1(:,2),'go','LineWidth',2,'MarkerSize',8);
+            end 
         end
         if isfield(mesh,'meas') == 1
             plot(mesh.meas.coord(:,1),...
@@ -43,20 +43,21 @@ if plotflag == 1
         end
         plot(mesh.nodes(ind,1),mesh.nodes(ind,2),'c.');
         axis equal;
-        legend('Source 1','Source 2','Sources +','Detector');
     elseif mesh.dimension == 3
         if isfield(mesh,'source') == 1
             tmp = sort(mesh.source.num);
             if length(tmp)>1
                 s1 = mesh.source.coord(mesh.source.num == tmp(1),:);
                 s2 = mesh.source.coord(mesh.source.num == tmp(2),:);
+                plot3(s1(:,1),s1(:,2),s1(:,3),'go',s2(:,1),s2(:,2),s2(:,3),'yo',...
+                    mesh.source.coord(3:end,1),...
+                    mesh.source.coord(3:end,2),...
+                    mesh.source.coord(3:end,3),'ro','LineWidth',2,'MarkerSize',8);
             else
-                s1 = mesh.source.coord; s2 = s1;
+                s1 = mesh.source.coord;
+                plot3(s1(:,1),s1(:,2),s1(:,3),'go','LineWidth',2,'MarkerSize',8);
             end
-            plot3(s1(:,1),s1(:,2),s1(:,3),'go',s2(:,1),s2(:,2),s2(:,3),'yo',...
-                mesh.source.coord(3:end,1),...
-                mesh.source.coord(3:end,2),...
-                mesh.source.coord(3:end,3),'ro','LineWidth',2,'MarkerSize',8);
+            
         end
         if isfield(mesh,'meas') == 1
             plot3(mesh.meas.coord(:,1),...
@@ -68,8 +69,17 @@ if plotflag == 1
             mesh.nodes(ind,2),...
             mesh.nodes(ind,3),'c.');
         axis equal;
-        legend('Source 1','Source 2','Sources +','Detector');
     end
+end
+
+if isfield(mesh,'source') && size(mesh.source.coord,1)>2
+    legend('Source 1','Source 2','Sources +','Detector');
+elseif isfield(mesh,'source') && size(mesh.source.coord,1)==2
+    legend('Source 1','Source 2','Detector');
+elseif isfield(mesh,'source') && size(mesh.source.coord,1)==1
+    legend('Source 1','Detector');
+else
+    legend('Detector');
 end
 
 %% plot optical properties
