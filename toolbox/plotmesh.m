3a14edc1052705bc3d7e6fa6d25c23d6b85202cb
jermynmichael@gmail.com <jermynmichael@gmail.com@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2012-04-18 18:30:18 +0000

    removed linux64 from nirviz plotmesh

--- toolbox/plotmesh.m
+++ toolbox/plotmesh.m
@@ -90,7 +90,7 @@ end
 os=computer;
 if mesh.dimension == 3 ... 
         && ~strcmp(mesh.type,'stnd_bem') ... % Not BEM mesh isempty(strfind(os,'GLNX')) ... % Not Linux && ( strcmpi(os,'maci') || ~isempty(strfind(os,'PCWIN')) ) ... % Not MAC64
-        && ~strcmp(mesh.type,'fluor_bem') && ~strcmp(mesh.type,'spec_bem')
+        && ~strcmp(mesh.type,'fluor_bem') && ~strcmp(mesh.type,'spec_bem') && ~strcmpi(os,'glnxa64')
     nirfast2vtk(mesh,'temp_nirfast.vtk');
     if ~isempty(strfind(os,'PCWIN64'))
         systemcall = ['"' which('nirviz.exe') '" temp_nirfast.vtk'];
@@ -98,7 +98,7 @@ if mesh.dimension == 3 ...
         systemcall = 'open -a nirviz-i386 temp_nirfast.vtk';
     elseif strcmpi(os,'maci64')
         systemcall = 'DYLD_FRAMEWORK_PATH=; open -a nirviz temp_nirfast.vtk';
-    elseif strcmpi(os,'glnx86') || strcmpi(os,'glnxa64')
+    elseif strcmpi(os,'glnx86')
         systemcall = ['"' GetSystemCommand('nirviz') '" temp_nirfast.vtk'];
     else
         error(['OS is not supported for 3D visualization: ' computer]);
