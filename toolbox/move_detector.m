c75a2db7af8902cedbc07dc1229e0e7f53fed929
jermynmichael@gmail.com <jermynmichael@gmail.com@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2012-05-09 17:23:47 +0000

    Fix for saving and loading the same mesh, which previously would re-move the sources/detectors

--- toolbox/move_detector.m
+++ toolbox/move_detector.m
@@ -22,6 +22,9 @@ if size(mesh.meas.coord,2) == 2
     remove_last = 1;
 end
 
+%% set fixed
+mesh.meas.fixed = 1;
+
 %% get list of boundary faces
 if size(mesh.elements,2) == 4
     faces = [mesh.elements(:,[1,2,3]);
