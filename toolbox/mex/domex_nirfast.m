ffe871d50f2fe705d92b5feaa0421d592ece8bb5
hamiid <hamiid@94dbad80-c818-323e-c6eb-ebfb85a78dd7>
2012-08-28 22:30:26 +0000

    Add LINUX defintion to mex compilation

--- toolbox/mex/domex_nirfast.m
+++ toolbox/mex/domex_nirfast.m
@@ -28,18 +28,18 @@ if isunix
         eval([mexcommand mexoptions ' -I./meshlib expand_bdybuffer_mex.cpp isinvolume_randRay.cpp meshlib/geomath.cpp meshlib/vector.cpp'])
         eval([mexcommand mexoptions ' CXXFLAGS="\$CXXFLAGS -fopenmp" LDFLAGS="\$LDFLAGS -fopenmp" orient3d_mex.cpp meshlib/geomath.cpp meshlib/vector.cpp meshlib/predicates.cpp -I./meshlib'])
     elseif ~isempty(strfind(os,'GLNX')) % Linux
-        eval([mexcommand mexoptions ' -Dlinux -I./meshlib tag_checkerboard3d_mex.cpp meshlib/CStopWatch.cpp'])
+        eval([mexcommand mexoptions ' -DLINUX -I./meshlib tag_checkerboard3d_mex.cpp meshlib/CStopWatch.cpp'])
         eval([mexcommand mexoptions ' -DLINUX -I./meshlib surface_relations_mex.cpp isinvolume_randRay.cpp meshlib/geomath.cpp meshlib/vector.cpp meshlib/polyhedron2BSP.cpp meshlib/CPoint.cpp meshlib/CVector.cpp meshlib/Plane3D.cpp meshlib/BSPNode.cpp meshlib/MeshIO.cpp meshlib/FileOperation.cpp meshlib/CPolygon.cpp meshlib/predicates.cpp meshlib/CStopWatch.cpp'])
         eval([mexcommand mexoptions ' pnpoly_mex.cpp'])
         eval([mexcommand mexoptions ' -DLINUX -I./meshlib orient_surface_mex.cpp meshlib/vector.cpp meshlib/geomath.cpp isinvolume_randRay.cpp'])
-        eval([mexcommand mexoptions ' -I./meshlib involume_mex.cpp isinvolume_randRay.cpp meshlib/geomath.cpp meshlib/vector.cpp'])
+        eval([mexcommand mexoptions ' -DLINUX -I./meshlib involume_mex.cpp isinvolume_randRay.cpp meshlib/geomath.cpp meshlib/vector.cpp'])
         eval([mexcommand mexoptions ' -DLINUX -I./meshlib intersect_ray_shell_mex.cpp ./meshlib/vector.cpp ./meshlib/geomath.cpp'])
         eval([mexcommand mexoptions ' extract_ind_regions_mex.cpp'])
         eval([mexcommand mexoptions ' -DLINUX -I./meshlib PointInPolyhedron_mex.cpp meshlib/polyhedron2BSP.cpp meshlib/CPoint.cpp meshlib/CVector.cpp meshlib/Plane3D.cpp meshlib/BSPNode.cpp meshlib/MeshIO.cpp meshlib/FileOperation.cpp meshlib/CPolygon.cpp meshlib/predicates.cpp meshlib/CStopWatch.cpp'])
-        eval([mexcommand mexoptions ' -I./meshlib GetListOfConnTri2Tri_mex.cpp meshlib/vector.cpp'])
-        eval([mexcommand mexoptions ' -I./meshlib get_ray_shell_intersections.cpp isinvolume_randRay.cpp meshlib/geomath.cpp meshlib/vector.cpp'])
-        eval([mexcommand mexoptions ' -I./meshlib expand_bdybuffer_mex.cpp isinvolume_randRay.cpp meshlib/geomath.cpp meshlib/vector.cpp'])
-        eval([mexcommand mexoptions ' -DLINUX CXXFLAGS="\$CXXFLAGS -fopenmp" LDFLAGS="\$LDFLAGS -fopenmp" -DLINUX orient3d_mex.cpp meshlib/geomath.cpp meshlib/vector.cpp meshlib/predicates.cpp -I./meshlib'])
+        eval([mexcommand mexoptions ' -DLINUX -I./meshlib GetListOfConnTri2Tri_mex.cpp meshlib/vector.cpp'])
+        eval([mexcommand mexoptions ' -DLINUX -I./meshlib get_ray_shell_intersections.cpp isinvolume_randRay.cpp meshlib/geomath.cpp meshlib/vector.cpp'])
+        eval([mexcommand mexoptions ' -DLINUX -I./meshlib expand_bdybuffer_mex.cpp isinvolume_randRay.cpp meshlib/geomath.cpp meshlib/vector.cpp'])
+        eval([mexcommand mexoptions ' -DLINUX CXXFLAGS="\$CXXFLAGS -fopenmp" LDFLAGS="\$LDFLAGS -fopenmp" orient3d_mex.cpp meshlib/geomath.cpp meshlib/vector.cpp meshlib/predicates.cpp -I./meshlib'])
     end
     
 else % PC
